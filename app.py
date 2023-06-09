4928dd723a7c33ad3f42c6b75c3715763bfad5d5
REKHA SUTHAR <71004640+rekha0suthar@users.noreply.github.com>
2023-06-09 13:34:26 +0530

    Added Prediction function

--- app.py
+++ app.py
@@ -30,5 +30,16 @@ def predict_api():
     return jsonify(output[0])
 
 
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = reg_model.predict(final_input)[0]
+    return render_template(
+        "home.html", prediction_text="The House Price prediction is {}".format(output)
+    )
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
